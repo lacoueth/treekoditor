86531afb12e382afda51972970e513c4de5c6081
Lacoueth <hugo.ribiere@gmail.com>
2018-12-15 01:36:46 +0100

    Try putting index.ts out of src folder

---
+++ index.ts
@@ -0,0 +1,10 @@
+import { _ra2ta } from './src/utils/ra2ta';
+import { _rt2ra } from './src/utils/rt2ra';
+import { _ta2pa, renderTTBlock } from './src/utils/ta2pa';
+import { _ta2rt } from './src/utils/ta2rt';
+
+export const ra2ta = _ra2ta;
+export const rt2ra = _rt2ra;
+export const ta2pa = _ta2pa;
+export const ta2rt = _ta2rt;
+export const pa2rh = renderTTBlock;
